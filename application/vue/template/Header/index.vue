48d2a3cb78a96dd9b9b162a35a6120417cb11b6d
andreisoroka <unickpro@gmail.com>
2018-03-12 01:46:08 +0300

    [header] add link to home and add title

--- application/vue/template/Header/index.vue
+++ application/vue/template/Header/index.vue
@@ -1,13 +1,39 @@
 <template>
   <div class="my-header">
-    <div class="my-header__logo"></div>
+    <router-link
+      to="/"
+      class="my-header__link">
+      <div class="my-header__logo"></div>
+    </router-link>
+    <h3 class="my-header__title">Concentration game</h3>
   </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+  .my-header {
+    margin: 10px 0;
+  }
+
+  .my-header__logo {
+    background: url("./logo.png") center center no-repeat;
+    width: 140px;
+    background-size: 100%;
+    height: 100px;
+    display: inline-block;
+    margin: 0 auto;
+  }
+
+  .my-header__title {
+    display: inline-block;
+  }
+
+  .my-header__link {
+    display: inline-block;
+    vertical-align: middle;
+  }
+</style>
 
 <script>
-  import './style.scss';
   import {mapState} from 'vuex';
 
   export default {
