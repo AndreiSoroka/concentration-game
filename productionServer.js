7a71a1b4cbfd6989eae2943bcc80177344fdeaac
andreisoroka <unickpro@gmail.com>
2018-03-14 05:29:18 +0300

    remove Nuxt

--- productionServer.js
+++ productionServer.js
@@ -1,7 +1,5 @@
 const express  = require('express');
 const path  = require('path');
-// import { Nuxt, Builder } from 'nuxt'
-
 
 const app = express();
 const host = process.env.HOST || '0.0.0.0';
@@ -12,17 +10,6 @@ app.set('port', port);
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use('*', express.static(path.join(__dirname, 'public')));
 
-// Import and Set Nuxt.js options
-// let config = require('../nuxt.config.js')
-
-// Init Nuxt.js
-// const nuxt = new Nuxt(config)
-
-
-// Give nuxt middleware to express
-// app.use(nuxt.render)
-
-// Listen the server
 app.listen(port, host);
 
-console.log('Server listening on ' + host + ':' + port); // eslint-disable-line no-console
+console.log('Server listening on ' + host + ':' + port);
